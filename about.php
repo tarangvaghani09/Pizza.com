8e5e9cb6b49b807319a1b464012c2f269118c572
tarangvaghani09 <132645667+tarangvaghani09@users.noreply.github.com>
2023-05-20 16:56:35 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,65 @@
+<?php
+include ("header.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ABOUT</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="media.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css"
+        integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body>
+    <!-- main section -->
+
+    <p class="about-title">Welcome to our pizza house</p>
+
+    <div class="container">
+    <div class="flex about-main">
+        <div class="a1">
+            <img src="images/a1.jpg" alt="" class="about-img">
+        </div>
+        <div class="about-txt flex">
+            <p class="about-txt1">Welcome! Lorem ipsum dolor sit  
+                  amet consectetur, adipisicing elit. Magni,
+                harum?
+                Consectetur, sapiente magnam quod ratione hic, necessitatibus voluptatem, facere debitis
+                aspernatur
+                deleniti quasi adipisci nam doloremque nisi consequuntur tempora nostrum laudantium? Veritatis
+                aperiam dolorum officia alias veniam laborum distinctio quam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, officia. veritatis accusantium. Architecto, perspiciatis?
+            </p>
+            </div>
+
+            <div class="a1">
+                <img src="images/a2.jpg" alt="" class="about-img">
+            </div>
+            <div class="about-txt flex">
+                <p class="about-txt1">Welcome! Lorem ipsum dolor sit  
+                    amet consectetur, adipisicing elit. Magni,
+                  harum?
+                  Consectetur, sapiente magnam quod ratione hic, necessitatibus voluptatem, facere debitis
+                  aspernatur
+                  deleniti quasi adipisci nam doloremque nisi consequuntur tempora nostrum laudantium? Veritatis
+                  aperiam dolorum officia alias veniam laborum distinctio quam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, officia. veritatis accusantium. Architecto, perspiciatis?
+              </p>
+                </div>
+        </div>
+    </div>
+
+    <?php
+    include ("footer.php");
+    ?>
+
+    <script src="new.js"></script>
+
+</body>
+
+</html>
